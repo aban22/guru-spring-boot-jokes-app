ef0cbb1da7bea56d57ce9d86ad67bbf72a51258f
Abel Fernandez <abelfergodevelop@gmail.com>
2019-05-12 21:45:37 +0200

    Create the project

---
+++ src/test/java/guru/springframework/guruspringbootjokesapp/GuruSpringBootJokesAppApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.springframework.guruspringbootjokesapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GuruSpringBootJokesAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
